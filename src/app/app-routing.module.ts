a695ab88744288d18bb7004bf1d92aba246b005b
EthanDuff <ethan.duff016@gmail.com>
2021-04-19 17:26:33 -0500

    Nothing special

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { EditNewsComponent } from './components/EditNews/edit-news/edit-news.component';
+import { AdminGuard } from './guards/admin.guard';
 
 const routes: Routes = [
   {
     path: 'contact',
     component: ContactUsComponent
+  },
+  {
+    path:'edit-news',
+    component: EditNewsComponent,
+    canActivate:[AdminGuard]
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { HeaderComponent } from './components/layout/header/header.component';
 import { NavBarComponent } from './components/layout/nav-bar/nav-bar.component';
 import { WeatherComponent } from './components/weather/weather.component';
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { EditNewsComponent } from './components/EditNews/edit-news/edit-news.component';
+import { EditNewsItemComponent } from './components/EditNews/edit-news-item/edit-news-item.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,9 @@ import { ContactUsComponent } from './components/contact-us/contact-us.component
     HeaderComponent,
     NavBarComponent,
     WeatherComponent,
-    ContactUsComponent
+    ContactUsComponent,
+    EditNewsComponent,
+    EditNewsItemComponent
   ],
   imports: [
     BrowserModule,
